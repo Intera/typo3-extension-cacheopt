ce5d6139b42e3c39da4be2202f6ff524652d3098
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-10-24 11:45:27 +0200

    [FEATURE] Default config for Extensions
    Default configuration is added for the cz_simple_cal and the
    news Extensions.

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,3 +16,31 @@ $signalSlotDispatcher->connect('TYPO3\\CMS\\Core\\Resource\\ResourceStorage', \T
 $signalSlotDispatcher->connect('TYPO3\\CMS\\Core\\Resource\\ResourceStorage', \TYPO3\CMS\Core\Resource\ResourceStorage::SIGNAL_PostFileCopy, 'Tx\\Cacheopt\\CacheOptimizerFiles', 'handleFileCopyPost');
 $signalSlotDispatcher->connect('TYPO3\\CMS\\Core\\Resource\\ResourceStorage', \TYPO3\CMS\Core\Resource\ResourceStorage::SIGNAL_PostFileSetContents, 'Tx\\Cacheopt\\CacheOptimizerFiles', 'handleFileSetContentsPost');
 $signalSlotDispatcher->connect('TYPO3\\CMS\\Core\\Resource\\ResourceStorage', \TYPO3\CMS\Core\Resource\ResourceStorage::SIGNAL_PostFileRename, 'Tx\\Cacheopt\\CacheOptimizerFiles', 'handleFileRenamePost');
+
+$cacheOptimizerRegistry = \Tx\Cacheopt\CacheOptimizerRegistry::getInstance();
+
+// Default configuration for the news Extension.
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
+
+	// Since news has its own cache handling we exclude the news records from further processing.
+	$cacheOptimizerRegistry->registerExcludedTable('tx_news_domain_model_news');
+	$cacheOptimizerRegistry->registerExcludedTable('tx_news_domain_model_tag');
+}
+
+// Default configuration for the cz_simple_cal Extension.
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('cz_simple_cal')) {
+
+	$cacheOptimizerRegistry->registerPluginForTables(
+		array(
+			'tx_czsimplecal_domain_model_address',
+			'tx_czsimplecal_domain_model_category',
+			'tx_czsimplecal_domain_model_event',
+		),
+		'czsimplecal_pi1'
+	);
+
+	// We do not want refindex traversal for the event index table.
+	$cacheOptimizerRegistry->registerExcludedTable('tx_czsimplecal_domain_model_eventindex');
+}
+
+unset($cacheOptimizerRegistry);
