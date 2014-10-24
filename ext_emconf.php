f08283a3ab7fe9af9fb7566f0a806994b7e67a28
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-10-24 13:26:15 +0200

    [TASK] Release version 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,50 +1,38 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "mediaoembed".
-#
-# Auto generated 16-06-2011 23:48
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cacheopt".
+ *
+ * Auto generated 24-10-2014 13:25
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Cache optimizer',
 	'description' => 'Optimizes automatic cache clearing.',
-	'category' => 'fe',
-	'shy' => 0,
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
+	'category' => 'be',
+	'version' => '1.0.1',
 	'state' => 'stable',
-	'internal' => 0,
-	'uploadfolder' => 0,
+	'uploadfolder' => false,
 	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => 'S',
+	'clearcacheonload' => true,
 	'author' => 'Alexander Stehlik',
 	'author_email' => 'astehlik.deleteme@intera.de',
 	'author_company' => 'Intera GmbH',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '1.0.0',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.1-6.2.99',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
-	),
-	'suggests' => array(
 	),
 );
 
-?>
