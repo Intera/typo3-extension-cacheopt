0a74463c263653861f88f62fe4393cf822116aa6
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-11-03 10:53:12 +0100

    [TASK] Update requirements in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "cacheopt".
- *
- * Auto generated 24-10-2014 13:53
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
+/** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache optimizer',
     'description' => 'Optimizes automatic cache clearing.',
@@ -22,18 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Stehlik',
     'author_email' => 'astehlik.deleteme@intera.de',
     'author_company' => 'Intera GmbH',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '6.2.1-7.99.99',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
-        ],
+    'constraints' => [
+        'depends' => ['typo3' => '6.2.1-8.7.99'],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
 ];
-
