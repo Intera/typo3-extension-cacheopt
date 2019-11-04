f10e5e530b507a10a67ddf9a05c73ae0e93e1d4d
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-11-04 22:06:47 +0100

    [TASK] Raise required TYPO3 version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache optimizer',
     'description' => 'Optimizes automatic cache clearing.',
     'category' => 'be',
-    'version' => '9.0.0',
+    'version' => '10.0.0-dev',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'astehlik.deleteme@intera.de',
     'author_company' => 'Intera GmbH',
     'constraints' => [
-        'depends' => ['typo3' => '9.5.0-9.5.99'],
+        'depends' => ['typo3' => '10.0.0-10.4.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
